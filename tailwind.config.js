c75a4044f0d71ade3c73048e1f4ab2248c120668
Sagar Kaurav <sagar.kaurav@gmail.com>
2020-07-25 09:05:08 +0530

    correct path for index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     purge: {
         enabled: process.env.NODE_ENV == "production",
         content: [
-            'index.html'
+            './public/index.html'
         ]
     },
     theme: {
